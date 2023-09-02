860d9b055239b488fa5496c74d66f9e3dcf6e662
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-09-02 11:51:02 -0300

    feature(bootstrap): created context bootstrapper

---
+++ Content.Bootstrap/Bootstrapping/ContextBootstrap.cs
@@ -0,0 +1,38 @@
+using Demen.Content.Data.Contexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Demen.Content.Bootstrap.Bootstrapping;
+
+public static class ContextBootstrap
+{
+	public static IServiceCollection ConfigureDbContext(
+		IServiceCollection services,
+		IConfiguration configuration
+	)
+	{
+		var contentConnectionString = configuration
+			.GetConnectionString(name: "Content");
+
+		services.AddDbContext<ContentDbContext>(options => options
+			.UseNpgsql(contentConnectionString)
+		);
+
+		return services;
+	}
+
+	public static void MigrateDatabaseOnStartUp(this IApplicationBuilder builder)
+	{
+		using var scope = builder
+			.ApplicationServices
+			.CreateAsyncScope();
+
+		using var contentContext = scope
+			.ServiceProvider
+			.GetRequiredService<ContentDbContext>();
+
+		contentContext.Database.Migrate();
+	}
+}
