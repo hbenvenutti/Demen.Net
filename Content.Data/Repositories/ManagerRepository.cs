78423e23c4f778865708a8c68754a45e26074537
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-09-02 23:40:25 -0300

    feature(data): created manager repository

---
+++ Content.Data/Repositories/ManagerRepository.cs
@@ -0,0 +1,31 @@
+using Demen.Content.Data.Contexts;
+using Demen.Content.Data.Entities;
+using Demen.Content.Domain.Manager;
+
+namespace Demen.Content.Data.Repositories;
+
+public class ManagerRepository : IManagerRepository
+{
+	private readonly ContentDbContext _contentDbContext;
+
+	// ---- constructors ---------------------------------------------------- //
+	public ManagerRepository(ContentDbContext contentDbContext)
+	{
+		_contentDbContext = contentDbContext;
+	}
+
+	// ---- methods --------------------------------------------------------- //
+	public async Task<ManagerDomain> CreateAsync(ManagerDomain managerDomain)
+	{
+		ManagerEntity managerEntity = managerDomain;
+
+		await _contentDbContext
+			.Managers
+			.AddAsync(managerEntity);
+
+		await _contentDbContext
+			.SaveChangesAsync();
+
+		return managerEntity;
+	}
+}
