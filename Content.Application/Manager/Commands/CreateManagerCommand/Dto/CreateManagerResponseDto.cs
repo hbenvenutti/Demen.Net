735b26e9faa405d4ca1669978dcc8b5c12d7b0f4
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-09-03 00:35:39 -0300

    feature: implemented command to create manager

--- Content.Application/Manager/Commands/CreateManagerCommand/Dto/CreateManagerResponseDto.cs
+++ Content.Application/Manager/Commands/CreateManagerCommand/Dto/CreateManagerResponseDto.cs
@@ -1,6 +1,26 @@
+using Demen.Content.Domain.Manager;
+
 namespace Demen.Content.Application.Manager.Commands.CreateManagerCommand.Dto;
 
 public class CreateManagerResponseDto
 {
-	
+	// ---- properties ------------------------------------------------------ //
+	public Guid Id { get; init; }
+	public required string Name { get; init; }
+	public required string Surname { get; init; }
+	public DateTime CreatedAt { get; init; }
+
+	// ---- operators ------------------------------------------------------- //
+	public static explicit operator CreateManagerResponseDto(
+		ManagerDomain managerDomain
+	)
+	{
+		return new CreateManagerResponseDto()
+		{
+			Id = managerDomain.ExternalId,
+			Name = managerDomain.Name,
+			Surname = managerDomain.Surname,
+			CreatedAt = managerDomain.CreatedAt
+		};
+	}
 }
