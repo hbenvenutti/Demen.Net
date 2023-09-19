63699ab3dd01c6213ba3b304c33c4f9d845d0c28
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-09-19 09:46:03 -0300

    refactor: renamed file

--- Content.Application/Error/EmailInUseError.cs
+++ Content.Application/Error/EmailInUseError.cs
@@ -2,12 +2,12 @@ using Demen.Content.Common.Enums;
 
 namespace Demen.Content.Application.Error;
 
-public class EmailInUSeError : ApplicationError
+public class EmailInUseError : ApplicationError
 {
 	private new const ErrorCode StatusCode = ErrorCode.BadData;
 	private new const string Message = "Email is already in use.";
 
-	public EmailInUSeError()
+	public EmailInUseError()
 		: base(StatusCode, Message)
 	{
 	}
