03f2779adc0711ffb612dff0a3d54d0e3bd41779
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-09-18 21:24:07 -0300

    refactor: changed namespaces

--- Content.Data/Migrations/20230903003922_Managers.cs
+++ Content.Data/Migrations/20230903003922_Managers.cs
@@ -4,40 +4,39 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Demen.Content.Data.Migrations
-{
-    /// <inheritdoc />
-    public partial class Managers : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "managers",
-                columns: table => new
-                {
-                    id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    name = table.Column<string>(type: "varchar", nullable: false),
-                    surname = table.Column<string>(type: "varchar", nullable: false),
-                    password = table.Column<string>(type: "varchar", nullable: false),
-                    external_id = table.Column<string>(type: "varchar", nullable: false),
-                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
-                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
-                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
-                    status = table.Column<string>(type: "varchar", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("id", x => x.id);
-                });
-        }
+namespace Demen.Content.Data.Migrations;
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "managers");
-        }
-    }
+/// <inheritdoc />
+public partial class Managers : Migration
+{
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.CreateTable(
+			name: "managers",
+			columns: table => new
+			{
+				id = table.Column<int>(type: "integer", nullable: false)
+					.Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+				name = table.Column<string>(type: "varchar", nullable: false),
+				surname = table.Column<string>(type: "varchar", nullable: false),
+				password = table.Column<string>(type: "varchar", nullable: false),
+				external_id = table.Column<string>(type: "varchar", nullable: false),
+				created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+				updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+				deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+				status = table.Column<string>(type: "varchar", nullable: false)
+			},
+			constraints: table =>
+			{
+				table.PrimaryKey("id", x => x.id);
+			});
+	}
+
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropTable(
+			name: "managers");
+	}
 }
--- Content.Data/Migrations/ContentDbContextModelSnapshot.cs
+++ Content.Data/Migrations/ContentDbContextModelSnapshot.cs
@@ -8,146 +8,145 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Demen.Content.Data.Migrations
+namespace Demen.Content.Data.Migrations;
+
+[DbContext(typeof(ContentDbContext))]
+partial class ContentDbContextModelSnapshot : ModelSnapshot
 {
-    [DbContext(typeof(ContentDbContext))]
-    partial class ContentDbContextModelSnapshot : ModelSnapshot
-    {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
+	protected override void BuildModel(ModelBuilder modelBuilder)
+	{
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.10")
-                .HasAnnotation("Relational:MaxIdentifierLength", 63);
+		modelBuilder
+			.HasAnnotation("ProductVersion", "7.0.10")
+			.HasAnnotation("Relational:MaxIdentifierLength", 63);
 
-            NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
+		NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
 
-            modelBuilder.Entity("Demen.Content.Data.Entities.EmailEntity", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("integer")
-                        .HasColumnName("id");
+		modelBuilder.Entity("Demen.Content.Data.Entities.EmailEntity", b =>
+		{
+			b.Property<int>("Id")
+				.ValueGeneratedOnAdd()
+				.HasColumnType("integer")
+				.HasColumnName("id");
 
-                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
+			NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
 
-                    b.Property<string>("Address")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("address");
+			b.Property<string>("Address")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("address");
 
-                    b.Property<DateTime>("CreatedAt")
-                        .HasColumnType("timestamp with time zone")
-                        .HasColumnName("created_at");
+			b.Property<DateTime>("CreatedAt")
+				.HasColumnType("timestamp with time zone")
+				.HasColumnName("created_at");
 
-                    b.Property<DateTime?>("DeletedAt")
-                        .HasColumnType("timestamp with time zone")
-                        .HasColumnName("deleted_at");
+			b.Property<DateTime?>("DeletedAt")
+				.HasColumnType("timestamp with time zone")
+				.HasColumnName("deleted_at");
 
-                    b.Property<string>("ExternalId")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("external_id");
+			b.Property<string>("ExternalId")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("external_id");
 
-                    b.Property<bool>("IsVerified")
-                        .HasColumnType("boolean")
-                        .HasColumnName("is_verified");
+			b.Property<bool>("IsVerified")
+				.HasColumnType("boolean")
+				.HasColumnName("is_verified");
 
-                    b.Property<int>("ManagerId")
-                        .HasColumnType("integer")
-                        .HasColumnName("manager_id");
+			b.Property<int>("ManagerId")
+				.HasColumnType("integer")
+				.HasColumnName("manager_id");
 
-                    b.Property<string>("StatusString")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("status");
+			b.Property<string>("StatusString")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("status");
 
-                    b.Property<string>("Type")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("type");
+			b.Property<string>("Type")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("type");
 
-                    b.Property<DateTime?>("UpdatedAt")
-                        .HasColumnType("timestamp with time zone")
-                        .HasColumnName("updated_at");
+			b.Property<DateTime?>("UpdatedAt")
+				.HasColumnType("timestamp with time zone")
+				.HasColumnName("updated_at");
 
-                    b.HasKey("Id")
-                        .HasName("pk_email_id");
+			b.HasKey("Id")
+				.HasName("pk_email_id");
 
-                    b.HasIndex("ManagerId");
+			b.HasIndex("ManagerId");
 
-                    b.ToTable("emails", (string)null);
-                });
+			b.ToTable("emails", (string)null);
+		});
 
-            modelBuilder.Entity("Demen.Content.Data.Entities.ManagerEntity", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("integer")
-                        .HasColumnName("id");
+		modelBuilder.Entity("Demen.Content.Data.Entities.ManagerEntity", b =>
+		{
+			b.Property<int>("Id")
+				.ValueGeneratedOnAdd()
+				.HasColumnType("integer")
+				.HasColumnName("id");
 
-                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
+			NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
 
-                    b.Property<DateTime>("CreatedAt")
-                        .HasColumnType("timestamp with time zone")
-                        .HasColumnName("created_at");
+			b.Property<DateTime>("CreatedAt")
+				.HasColumnType("timestamp with time zone")
+				.HasColumnName("created_at");
 
-                    b.Property<DateTime?>("DeletedAt")
-                        .HasColumnType("timestamp with time zone")
-                        .HasColumnName("deleted_at");
+			b.Property<DateTime?>("DeletedAt")
+				.HasColumnType("timestamp with time zone")
+				.HasColumnName("deleted_at");
 
-                    b.Property<string>("ExternalId")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("external_id");
+			b.Property<string>("ExternalId")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("external_id");
 
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("name");
+			b.Property<string>("Name")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("name");
 
-                    b.Property<string>("Password")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("password");
+			b.Property<string>("Password")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("password");
 
-                    b.Property<string>("StatusString")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("status");
+			b.Property<string>("StatusString")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("status");
 
-                    b.Property<string>("Surname")
-                        .IsRequired()
-                        .HasColumnType("varchar")
-                        .HasColumnName("surname");
+			b.Property<string>("Surname")
+				.IsRequired()
+				.HasColumnType("varchar")
+				.HasColumnName("surname");
 
-                    b.Property<DateTime?>("UpdatedAt")
-                        .HasColumnType("timestamp with time zone")
-                        .HasColumnName("updated_at");
+			b.Property<DateTime?>("UpdatedAt")
+				.HasColumnType("timestamp with time zone")
+				.HasColumnName("updated_at");
 
-                    b.HasKey("Id")
-                        .HasName("pk_manager_id");
+			b.HasKey("Id")
+				.HasName("pk_manager_id");
 
-                    b.ToTable("managers", (string)null);
-                });
+			b.ToTable("managers", (string)null);
+		});
 
-            modelBuilder.Entity("Demen.Content.Data.Entities.EmailEntity", b =>
-                {
-                    b.HasOne("Demen.Content.Data.Entities.ManagerEntity", "Manager")
-                        .WithMany("Emails")
-                        .HasForeignKey("ManagerId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired()
-                        .HasConstraintName("fk_manager_id");
+		modelBuilder.Entity("Demen.Content.Data.Entities.EmailEntity", b =>
+		{
+			b.HasOne("Demen.Content.Data.Entities.ManagerEntity", "Manager")
+				.WithMany("Emails")
+				.HasForeignKey("ManagerId")
+				.OnDelete(DeleteBehavior.Cascade)
+				.IsRequired()
+				.HasConstraintName("fk_manager_id");
 
-                    b.Navigation("Manager");
-                });
+			b.Navigation("Manager");
+		});
 
-            modelBuilder.Entity("Demen.Content.Data.Entities.ManagerEntity", b =>
-                {
-                    b.Navigation("Emails");
-                });
+		modelBuilder.Entity("Demen.Content.Data.Entities.ManagerEntity", b =>
+		{
+			b.Navigation("Emails");
+		});
 #pragma warning restore 612, 618
-        }
-    }
+	}
 }
