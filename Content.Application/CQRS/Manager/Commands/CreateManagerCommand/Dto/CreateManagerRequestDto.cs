a614ae962464f408fa5d0deefd362f40491a9fbc
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-09-18 23:45:50 -0300

    feature: email creation alongside with manager

--- Content.Application/CQRS/Manager/Commands/CreateManagerCommand/Dto/CreateManagerRequestDto.cs
+++ Content.Application/CQRS/Manager/Commands/CreateManagerCommand/Dto/CreateManagerRequestDto.cs
@@ -1,3 +1,5 @@
+using Demen.Content.Common.Enums;
+using Demen.Content.Common.Helpers;
 using Demen.Content.Domain.Manager;
 
 namespace Demen.Content.Application.CQRS.Manager.Commands.CreateManagerCommand.Dto;
@@ -7,6 +9,8 @@ public class CreateManagerRequestDto
 	public required string Name { get; init; }
 	public required string Surname { get; init; }
 	public required string Password { get; init; }
+	public required string Email { get; init; }
+	public string? EmailType { get; init; }
 
 	// ---- operators ------------------------------------------------------- //
 	public static implicit operator ManagerDomain(CreateManagerRequestDto dto)
@@ -14,7 +18,9 @@ public class CreateManagerRequestDto
 		return ManagerDomain.Create(
 			name: dto.Name,
 			surname: dto.Surname,
-			password: dto.Password
+			password: dto.Password,
+			email: dto.Email,
+			emailType: dto.EmailType?.StringToEnum<EmailType>()
 		);
 	}
 }
