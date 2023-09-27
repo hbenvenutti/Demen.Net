94b47260d225270dcd5ef2fce6b74f2320c680c3
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-09-27 13:43:09 -0300

    feature: removed password from manager domain

--- Domain/Management/Manager/ManagerDomain.cs
+++ Domain/Management/Manager/ManagerDomain.cs
@@ -10,32 +10,9 @@ public class ManagerDomain : BaseDomain
 	// ---- properties ------------------------------------------------------ //
 	public required string Name { get; init; }
 	public required string Surname { get; init; }
-	public required string Password { get; init; }
 
 	// ---- relationships --------------------------------------------------- //
 
-	public ICollection<EmailDomain>? Emails { get; init; }
-	public ICollection<VideoDomain>? Videos { get; init; }
-
-	// ---- factories ------------------------------------------------------- //
-
-	public static ManagerDomain Create(
-		string name,
-		string surname,
-		string password
-	) =>
-		new()
-		{
-			Id = 0,
-			ExternalId = Guid.Empty,
-			Status = Status.Active,
-			CreatedAt = DateTime.UtcNow,
-			UpdatedAt = null,
-			DeletedAt = null,
-			Name = name,
-			Surname = surname,
-			Password = password,
-			Emails = null,
-			Videos = null
-		};
+	public ICollection<EmailDomain>? Emails { get; set; }
+	public ICollection<VideoDomain>? Videos { get; set; }
 }
