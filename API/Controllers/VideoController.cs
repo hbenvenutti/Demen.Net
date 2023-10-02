ab7871d875e62a4a0662e2b7d7967db1be40cb1f
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-10-02 18:13:02 -0300

    refactor: file scoped namespace

--- API/Controllers/VideoController.cs
+++ API/Controllers/VideoController.cs
@@ -5,77 +5,76 @@ using Demen.Application.Dto;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Demen.API.Controllers
+namespace Demen.API.Controllers;
+
+[ApiController]
+[Route(template: "api/v1/videos")]
+[Produces(contentType: "application/json")]
+
+public class VideoController : ControllerBase
 {
-    [ApiController]
-    [Route(template: "api/v1/videos")]
-    [Produces(contentType: "application/json")]
+	private readonly IMediator _mediator;
 
-    public class VideoController : ControllerBase
-    {
-	    private readonly IMediator _mediator;
+	public VideoController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
 
-	    public VideoController(IMediator mediator)
-	    {
-		    _mediator = mediator;
-	    }
+	// ---- routes ------------------------------------------------------ //
 
-	    // ---- routes ------------------------------------------------------ //
+	[HttpPost]
+	[ProducesResponseType(
+		statusCode: StatusCodes.Status201Created,
+		type: typeof(Response<>)
+	)]
 
-	    [HttpPost]
-	    [ProducesResponseType(
-		    statusCode: StatusCodes.Status201Created,
-		    type: typeof(Response<>)
-	    )]
+	[ProducesResponseType(
+		statusCode: StatusCodes.Status400BadRequest,
+		type: typeof(Response<>)
+	)]
 
-	    [ProducesResponseType(
-		    statusCode: StatusCodes.Status400BadRequest,
-		    type: typeof(Response<>)
-	    )]
+	[ProducesResponseType(
+		statusCode: StatusCodes.Status500InternalServerError,
+		type: typeof(Response<EmptyDto>)
+	)]
 
-	    [ProducesResponseType(
-		    statusCode: StatusCodes.Status500InternalServerError,
-		    type: typeof(Response<EmptyDto>)
-	    )]
+	public async Task<IActionResult> CreateVideo(CreateVideoRequest request)
+	{
 
-	    public async Task<IActionResult> CreateVideo(CreateVideoRequest request)
-	    {
+		var response = await _mediator.Send(request);
 
-		    var response = await _mediator.Send(request);
+		if (!response.IsSuccess)
+			return StatusCode(
+				statusCode: response.HttpStatusCode,
+				value: response
+			);
 
-		    if (!response.IsSuccess)
-				return StatusCode(
-					statusCode: response.HttpStatusCode,
-					value: response
-				);
+		return CreatedAtAction(
+			actionName: nameof(GetVideoById),
+			routeValues: new { id = response.Data!.Id },
+			value: response
+		);
+	}
 
-		    return CreatedAtAction(
-			    actionName: nameof(GetVideoById),
-			    routeValues: new { id = response.Data!.Id },
-			    value: response
-		    );
-	    }
-
-	    [HttpGet(template: "{id:Guid}")]
-	    public async Task<IActionResult> GetVideoById(
-	    [FromRoute] Guid id,
+	[HttpGet(template: "{id:Guid}")]
+	public async Task<IActionResult> GetVideoById(
+		[FromRoute] Guid id,
 		[FromQuery] bool channel = false,
 		[FromQuery] bool manager = false
-		)
-	    {
-		    var request = new FindVideoRequest
-		    {
-			    Id = id,
-			    IncludeChannel = channel,
-			    IncludeManager = manager
-		    };
+	)
+	{
+		var request = new FindVideoRequest
+		{
+			Id = id,
+			IncludeChannel = channel,
+			IncludeManager = manager
+		};
 
-		    var result = await _mediator.Send(request);
+		var result = await _mediator.Send(request);
 
-		    return StatusCode(
-			    statusCode: result.HttpStatusCode,
-			    value: result
-		    );
-	    }
-    }
+		return StatusCode(
+			statusCode: result.HttpStatusCode,
+			value: result
+		);
+	}
 }
